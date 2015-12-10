f29327093804484f397150292c4b68dc059a2ddf
lisnb <lisnb.h@gmail.com>
2015-12-10 09:44:25 +0800

    Add favicon update some layouts

--- conitor.js/app.js
+++ conitor.js/app.js
@@ -18,7 +18,7 @@ app.set('view engine', 'html');
 app.engine('html', swig.renderFile)
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
